ae793d4a4a4686fcb7ff8bc121f963f52fb0241d
sugune-sukira <cymine24@gmail.com>
2022-08-20 12:55:16 +0800

    changes

--- mech.js
+++ mech.js
@@ -2,7 +2,24 @@ const cartBtn = document.querySelector('.cart-btn');
 const xbtn = document.querySelector('.fa-xmark');
 
 const menuBtn = document.querySelector('.menu-btn');
-const navMenu = document.querySelector('.nav-menu')
+const navMenu = document.querySelector('.nav-menu');
+
+const cartUp = document.querySelectorAll('.add-cart');
+const cartDown = document.querySelectorAll('.remove-cart');
+
+cartUp.forEach(add => {
+  add.addEventListener('click', () => {
+    add.classList.add('hide-cart-up');
+    add.previousElementSibling.classList.add('show-cart-down');
+  });
+})
+
+cartDown.forEach(remove => {
+  remove.addEventListener('click', () => {
+    remove.classList.remove('show-cart-down');
+    remove.nextElementSibling.classList.remove('hide-cart-up');
+  });
+})
 
 cartBtn.addEventListener('click', () => {
   cartBtn.classList.add('hide-cart-btn');
@@ -16,7 +33,7 @@ xbtn.addEventListener('click', () => {
 
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('active');
-  navMenu.classList.toggle('show-menu')
+  navMenu.classList.toggle('show-menu');
 });
 
 
